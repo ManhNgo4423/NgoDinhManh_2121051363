adb156e8e0abdc19a23f577b0a0ee5d11bc6c1e9
“ManhNgo4423” <manhngo04042003@gmail.com>
2025-11-06 13:36:35 +0700

    BTTHBTTH04

--- MvcMovie363/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie363/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd5ddd3f67d99c5152316656d7ce04cce27cb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fcab7cd57d482b0e65a6ff3bc54adb8d323802")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
